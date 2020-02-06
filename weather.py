3f26734f23fc79d659a84097404e7bd4b116663a
aseplyaev <sanjok126@yandex.ru>
2020-02-06 18:30:16 +0300

    commenting code

--- weather.py
+++ weather.py
@@ -1,7 +1,13 @@
 import requests
 from utils import get_param
 
-def weather_by_city(city_name):
+
+def weather_by_city(city_name: str):
+    """
+    Возвращает данные о погоде в заданом городе.
+    :param city_name: Название города.
+    :return dict: Данные о погоде.
+    """
     url = "http://api.openweathermap.org/data/2.5/weather"
     params = {
         "q": city_name,
