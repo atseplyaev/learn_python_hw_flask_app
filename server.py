133d0864eca86d05d7afb92c0bdc386725483db2
aseplyaev <sanjok126@yandex.ru>
2020-02-07 08:25:30 +0300

    add rendering template
    Обновил логику генерирования главной страницы, добавив рендеринг html из
    шаблона.

--- server.py
+++ server.py
@@ -1,16 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 from weather import weather_by_city
+from news import get_python_news
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
+    title = "Новости Python"
     weather = weather_by_city("Ryazan")
-    if weather:
-        return f"Сейчас {weather['main']['temp']}, ощущается как {weather['main']['feels_like']}"
-    else:
-        return "Прогноз погоды сейчас недоступен"
+    news = get_python_news()
+    return render_template('index.html', page_title=title, weather=weather, news=news)
 
 
 if __name__ == '__main__':
