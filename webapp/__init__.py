6bc429df8391ae62d8e206a0b4eb42d0a9babcc1
aseplyaev <sanjok126@yandex.ru>
2020-02-07 09:53:07 +0300

    add fabric-method for init webapp
    Вынес логику инициализации приложения в отдельную функцию.

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,16 +2,18 @@ from flask import Flask, render_template
 from webapp.weather import weather_by_city
 from webapp.news import get_python_news
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
+    @app.route('/')
+    def hello():
+        title = "Новости Python"
+        weather = weather_by_city("Ryazan")
+        news = get_python_news()
+        return render_template('index.html', page_title=title, weather=weather, news=news)
 
-@app.route('/')
-def hello():
-    title = "Новости Python"
-    weather = weather_by_city("Ryazan")
-    news = get_python_news()
-    return render_template('index.html', page_title=title, weather=weather, news=news)
-
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run()
