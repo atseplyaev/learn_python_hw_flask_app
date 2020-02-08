35ab8d9ca424a69c437214d4eb651993391e92b0
aseplyaev <sanjok126@yandex.ru>
2020-02-08 13:04:19 +0300

    add secret key

--- webapp/config.py
+++ webapp/config.py
@@ -7,3 +7,5 @@ SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "..", "webapp.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 WEATHER_DEFAULT_CITY = "Ryazan"
 WEATHER_API_KEY = get_param("weather_api", "APPID")
+SECRET_KEY = os.urandom(24)
+
