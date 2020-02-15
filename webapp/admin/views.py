e1d27c04833f948f93dc60c62cd4c99f0d395375
aseplyaev <sanjok126@yandex.ru>
2020-02-15 12:08:01 +0300

    add decorator admin_required
    Signed-off-by: aseplyaev <sanjok126@yandex.ru>

--- webapp/admin/views.py
+++ webapp/admin/views.py
@@ -1,10 +1,11 @@
-from flask import Blueprint
-from flask_login import current_user, login_required
+from flask import Blueprint, render_template
+from flask_login import current_user
+from webapp.user.decorators import admin_required
 bp = Blueprint("admin", __name__, url_prefix="/admin")
 
 
-@bp.route("/admin")
-@login_required
+@bp.route("/")
+@admin_required
 def admin_index():
     if current_user.is_admin:
         return "Привет админ"
