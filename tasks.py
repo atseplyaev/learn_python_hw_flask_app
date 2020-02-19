1c66281ebb66898812d0b34331f05de222de00e2
aseplyaev <sanjok126@yandex.ru>
2020-02-19 07:40:42 +0300

    add tasks
    Signed-off-by: aseplyaev <sanjok126@yandex.ru>

---
+++ tasks.py
@@ -0,0 +1,24 @@
+from celery import Celery
+from celery.schedules import crontab
+from webapp import create_app
+from webapp.news.parsers import habr
+
+flask_app = create_app()
+celery_app = Celery("webapp.tasks", broker="redis://localhost:6379/0")
+celery_app.conf.CELERYD_MAX_TASKS_PER_CHILD = 1
+
+@celery_app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(crontab(minute='*/1'), habr_snippets.s())
+    sender.add_periodic_task(crontab(minute='*/2'), habr_content.s())
+
+@celery_app.task
+def habr_snippets():
+    with flask_app.app_context():
+        habr.get_news_snippets()
+
+
+@celery_app.task
+def habr_content():
+    with flask_app.app_context():
+        habr.get_news_content()
